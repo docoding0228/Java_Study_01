35c1a41952dc5d17f37d0dd99701a6884eb3cf9f
docoding0228 <npng0228@naver.com>
2024-04-24 14:33:37 +0900

    연산자 예제

--- src/week02/w02_2.java
+++ src/week02/w02_2.java
@@ -1,6 +1,11 @@
 package week02;
 
 public class w02_2 {
-
-
+public static void main(String[] args) {
+    
+    System.out.println("1"); // 10
+    System.out.println("1"); // 10
+    System.out.println("1"); // 10 c는 연산 후에 -1
+    System.out.println("1"); // 11 d는 연산 전에 +1
+    }
 }
