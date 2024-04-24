5154a3fdd124ff5a66cdf37d9962d2208eaeb852
docoding0228 <npng0228@naver.com>
2024-04-24 14:40:33 +0900

    깃 연동 테스트

--- src/week02/w02_2.java
+++ src/week02/w02_2.java
@@ -7,5 +7,6 @@ public static void main(String[] args) {
     System.out.println("1"); // 10
     System.out.println("1"); // 10 c는 연산 후에 -1
     System.out.println("1"); // 11 d는 연산 전에 +1
+    System.out.println("1"); // 11 d는 연산 전에 +1
     }
 }
