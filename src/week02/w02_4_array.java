13b3678dedefa1c99119ae88406918d12acb9aa1
docoding0228 <npng0228@naver.com>
2024-04-25 19:21:49 +0900

    string 배열 예제 추가

--- src/week02/w02_4_array.java
+++ src/week02/w02_4_array.java
@@ -69,30 +69,62 @@ public class w02_4_array {
 //        System.out.println(Arrays.toString(b));
 //        // 배열의 내용을 출력하려면 Arrays.toString()을 사용해야 한다.
 
-        String str = "ABCD";
-        // length()
-        int strLength = str.length();
-        System.out.println(strLength);  // 4 출력
+//        String str = "ABCD";
+//        // length()
+//        int strLength = str.length();
+//        System.out.println(strLength);  // 4 출력
+//
+//        // charAt(int index)
+//        char strChar = str.charAt(2); // 순번은 0부터 시작하니까 2순번은 3번째 문자를 가리킨다.
+//        System.out.println(strChar);  // C 출력
+//
+//        // substring(int from, int to)
+//        String strSub = str.substring(0, 3); // 0~2순번까지 자르기 합니다. (3순번은 제외)
+//        System.out.println(strSub);  // ABC 출력
+//
+//        // equals(String str)
+//        String newStr = "ABCE";  // str 값과 같은 문자열 생성
+//        boolean strEqual = newStr.equals(str);
+//        System.out.println(strEqual); // false 출력
+//
+//        // toCharArray()
+//        char[] strCharArray = str.toCharArray(); // String 을 char[] 로 변환
+//
+//        // 반대로 char[] 를 String로 변환하는 방법
+//        char[] charArray = {'A', 'B', 'C'};
+//        String charArrayString = new String(charArray); // char[] 를 String 으로 변환
 
-        // charAt(int index)
-        char strChar = str.charAt(2); // 순번은 0부터 시작하니까 2순번은 3번째 문자를 가리킵니다.
-        System.out.println(strChar);  // C 출력
+//        // 2차원 배열
+//        int[][] array = {
+//                {1, 2, 3},
+//                {4, 5, 6}
+//        };
 
-        // substring(int from, int to)
-        String strSub = str.substring(0, 3); // 0~2순번까지 자르기 합니다. (3순번은 제외)
-        System.out.println(strSub);  // ABC 출력
+//        int[][] array = new int[2][3];
+//
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array[i].length; j++) {
+//                System.out.println("출력값 : " + i + " , " + j);
+//                array[i][j] = 0;  // i, j 는 인덱스 라고 부릅니다.
+//            }
+//        }
 
-        // equals(String str)
-        String newStr = "ABCE";  // str 값과 같은 문자열 생성
-        boolean strEqual = newStr.equals(str);
-        System.out.println(strEqual); // false 출력
+//        // 가변 배열
+//        int[][] array = new int[3][];
+//        // 배열 원소마다 각기다른 크기로 지정 가능.
+//        array[0] = new int[2];
+//        array[1] = new int[4];
+//        array[2] = new int[1];
+//
+//        // 중괄호 초기화할때도 원소배열들의 크기를 각기 다르게 생성 가능.
+//        int[][] array2 = {
+//                {10, 20},
+//                {10, 20, 30, 40},
+//                {10}
+//        };
 
-        // toCharArray()
-        char[] strCharArray = str.toCharArray(); // String 을 char[] 로 변환
-
-        // 반대로 char[] 를 String로 변환하는 방법
-        char[] charArray = {'A', 'B', 'C'};
-        String charArrayString = new String(charArray); // char[] 를 String 으로 변환
-
+          // 3차원 배열의 이해
+          // 중괄호 3개를 써서 3차원 배열 초기화를 할 수 있음.
+          int[][][] MultiArray = {{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}};
     }
 }
