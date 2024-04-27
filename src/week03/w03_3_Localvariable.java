70d997ddf64cee24be010a89d7a11504a9185b66
docoding0228 <npng0228@naver.com>
2024-04-28 00:33:36 +0900

    지역변수 예제 실습

---
+++ src/week03/w03_3_Localvariable.java
@@ -0,0 +1,23 @@
+package week03;
+
+public class w03_3_Localvariable {
+    public static void main(String[] args) {
+        w03_3_Localvariable main = new w03_3_Localvariable();
+
+        // 메서드 호출 : main.getClass()
+        System.out.println("main.getClass() = " + main.getNumber());
+        System.out.println("main.getClass() = " + main.getNumber());
+        System.out.println("main.getClass() = " + main.getNumber());
+    }
+
+    public int getNumber() {
+        int number = 1; // 지역 변수
+        number += 1;
+        return number; // 메서드 종료되면 지역변수 제거됨
+    }
+}
+
+// 출력
+//main.getNumber() = 2
+//main.getNumber() = 2
+//main.getNumber() = 2
