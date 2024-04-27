26840c4de42becf35590eb1df8c3adb046804bfd
docoding0228 <npng0228@naver.com>
2024-04-28 00:35:58 +0900

    클래스 멤버 예제 실습

---
+++ src/week03/w03_2_main.java
@@ -0,0 +1,25 @@
+package week03;
+
+public class w03_2_main {
+    public static void main(String[] args) {
+        // 클래스 필드 company 확인
+        System.out.println(w03_2_car.company + "\n");
+        // 클래스 필드 변경 및 확인
+        w03_2_car.company = "Audi";
+        System.out.println(w03_2_car.company + "\n");
+
+        // 클래스 메서드 호출
+        String companyName = w03_2_car.setCompany("Benz");
+        System.out.println("companyName = " + companyName);
+
+        System.out.println();
+        // 참조형 변수 사용
+        w03_2_car car = new w03_2_car(); // 객체 생성
+
+        car.company = "Ferrari";
+        System.out.println(car.company + "\n");
+
+        String companyName2 = car.setCompany("Lamborghini");
+        System.out.println("companyName2 = " + companyName2);
+    }
+}
