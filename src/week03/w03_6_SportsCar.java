8441f5e56329d45cfbca992f10a72b0296ed1272
docoding0228 <npng0228@naver.com>
2024-04-29 17:54:06 +0900

    상속 예제 실습

---
+++ src/week03/w03_6_SportsCar.java
@@ -0,0 +1,8 @@
+package week03;
+
+public class w03_6_SportsCar extends w03_6_inheritance{
+    String engine;
+    public void booster() {
+        System.out.println("엔진 " + engine + " 부앙~\n");
+    }
+}
---
+++ src/week03/w03_6_main.java
@@ -0,0 +1,25 @@
+package week03;
+
+    public class w03_6_main {
+        public static void main(String[] args) {
+            // 부모 클래스 객체에서 자식 클래스 멤버 사용
+            w03_6_inheritance car = new w03_6_inheritance();
+            // car.engine = "Orion"; // 오류
+            // car.booster(); // 오류
+
+            // 자식 클래스 객체 생성
+            w03_6_SportsCar sportsCar = new w03_6_SportsCar();
+            sportsCar.engine = "Orion";
+            sportsCar.booster();
+
+            // 자식 클래스 객체에서 부모 클래스 멤버 사용
+            sportsCar.company = "GENESIS";
+            sportsCar.setModel("GV80");
+            System.out.println("sportsCar.company = " + sportsCar.company);
+            System.out.println("sportsCar.getModel() = " + sportsCar.getModel());
+            System.out.println();
+
+            sportsCar.horn();
+            System.out.println(sportsCar.changeGear('D'));
+        }
+    }
