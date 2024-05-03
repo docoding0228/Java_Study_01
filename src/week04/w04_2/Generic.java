fc20c22d8c65c41eedff66227df7a4972f644820
docoding0228 <npng0228@naver.com>
2024-05-03 19:57:51 +0900

    Generic 실습 예제

---
+++ src/week04/w04_2/Generic.java
@@ -0,0 +1,13 @@
+package week04.w04_2;
+
+public class Generic {
+    public String plusReturnFunction(int a, int b) { ... }
+
+    public String plusReturnFunction(int a, long b) { ... }
+
+    public String plusReturnFunction(int a, String b) { ... }
+}
+
+//public class Generic {
+//    public Object plusReturnFunction(Object a,Object b) { ... }
+//}
