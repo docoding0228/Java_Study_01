98ebde1fc6a1f0d296ccaea4118a23092c289b4c
docoding0228 <npng0228@naver.com>
2024-05-03 19:59:29 +0900

    Generic 실습 예제

--- src/week04/w04_2/Generic.java
+++ src/week04/w04_2/Generic.java
@@ -1,13 +1,39 @@
 package week04.w04_2;
 
 public class Generic {
-    public String plusReturnFunction(int a, int b) { ... }
-
-    public String plusReturnFunction(int a, long b) { ... }
-
-    public String plusReturnFunction(int a, String b) { ... }
+//    public String plusReturnFunction(int a, int b) { ... }
+//
+//    public String plusReturnFunction(int a, long b) { ... }
+//
+//    public String plusReturnFunction(int a, String b) { ... }
+//}
 }
 
 //public class Generic {
 //    public Object plusReturnFunction(Object a,Object b) { ... }
+//}
+
+// 1.
+//public class Generic<T> {
+//    // 2.
+//    private T t;
+//    // 3.
+//    public T get() {
+//        return this.t;
+//    }
+//
+//    public void set(T t) {
+//        this.t = t;
+//    }
+//
+//    public static void main(String[] args) {
+//        // 4.
+//        Generic<String> stringGeneric = new Generic<>();
+//        // 5.
+//        stringGeneric.set("Hello World");
+//
+//        String tValueTurnOutWithString = stringGeneric.get();
+//
+//        System.out.println(tValueTurnOutWithString);
+//    }
 //}
