42619ce4357909fb28b6ae35281fe572bcb725ce
docoding0228 <npng0228@naver.com>
2024-05-07 02:53:03 +0900

    my team Student 수정

---
+++ src/my_team/Student.java
@@ -0,0 +1,21 @@
+package my_team;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Student {
+
+    private static String studentId = "";
+    private static String studentName = "";
+
+    public void setStudentId(String studentId){ this.studentId = studentId; }
+    public void setStudentName(String studentName){ this.studentName = studentName; }
+
+}
+
+//    public static String pushID() {
+//    System.out.print("수강생 ID를 입력하세요: ");
+//    String studentId2 = sc.next();
+//    return studentId2;
+//}
