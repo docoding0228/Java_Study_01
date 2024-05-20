c51bb1f188a84d58495c57980aadbc6030af3e89
docoding0228 <npng0228@naver.com>
2024-05-20 14:13:58 +0900

    Generics 예제1

---
+++ src/week5/Generics1/Box.java
@@ -0,0 +1,13 @@
+package week5.Generics1;
+
+public class Box<T> {
+    private T item;
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+}
--- src/week5/Generics1/Main.java
+++ src/week5/Generics1/Main.java
@@ -1,4 +1,23 @@
 package week5.Generics1;
 
 public class Main {
+    public static void main(String[] args) {
+        // 문자열을 위한 Box
+        Box<String> stringBox = new Box<>();
+        stringBox.setItem("Hello, World!");
+        String str = stringBox.getItem();
+        System.out.println("박스에 있는 문자열: " + str);
+
+        // 정수를 위한 Box
+        Box<Integer> integerBox = new Box<>();
+        integerBox.setItem(100);
+        Integer num = integerBox.getItem();
+        System.out.println("박스에 있는 정수: " + num);
+
+        // 커스텀 타입을 위한 Box
+        Box<Person> personBox = new Box<>();
+        personBox.setItem(new Person("John", 30));
+        Person person = personBox.getItem();
+        System.out.println("박스에 있는 사람: " + person.getName() + ", 나이: " + person.getAge());
+    }
 }
